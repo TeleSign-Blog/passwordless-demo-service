efd6fe318c3ba76340c93823b6e1d4b429d3a784
Luka Zitnik <lzitnik@telesign.com>
2017-02-10 02:04:33 +0100

    make client-side jwt session, serve albums

--- public/app.php
+++ public/app.php
@@ -3,6 +3,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use \Firebase\JWT\JWT;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App([
@@ -26,9 +28,37 @@ $app->get('/ping', function (Request $request, Response $response) {
   return $response->write('pong');
 });
 
-$app->post('/api/authenticate', function (Request $request, Response $response) {
+$app->get('/api/authenticate', function (Request $request, Response $response) {
+  $params = $request->getQueryParams();
+  $phone = $params['phone'];
+  $expires_in = 5 * 60;
+  $payload = [
+    'sub' => $phone,
+    'exp' => time() + $expires_in
+  ];
+
   // TODO Call TeleSign SDK
-  return $response->write('');
+
+  $jwt = JWT::encode($payload, 'session_secret_key');
+
+  return $response->withJson([
+    'id_token' => $jwt,
+    'token_type' => 'Bearer',
+    'expires_in' => $expires_in
+  ]);
+});
+
+$app->group('/api/protected', function () use ($app) {
+  $app->get('/albums', function (Request $request, Response $response) {
+    return $response
+      ->withHeader('Content-Type', 'application/json')
+      ->write(file_get_contents(__DIR__ . '/../albums.json'));
+  });
+})->add(function (Request $request, Response $response, callable $next) {
+
+  // TODO Validate jwt
+
+  return $next($request, $response);
 });
 
 $app->run();
